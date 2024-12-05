bdbbb421ab7c709ee3447467b586b74e524c448f
YiChuanWang <1425837933@qq.com>
2024-12-05 16:24:58 +0800

    删除菜品功能实现

---
+++ sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
@@ -0,0 +1,15 @@
+package com.sky.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface SetmealDishMapper {
+    /**
+     * 根据多个菜品id查询多个套餐id
+     * @param dishIds
+     * @return
+     */
+    List<Long> getSetmealDishIdsByDishIds(List<Long> dishIds);
+}
